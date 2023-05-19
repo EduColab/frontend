bd35d0cf30afe843ceaf45571725591e324eedf5
eufanzky <eufanzky@gmail.com>
2023-05-19 17:05:46 -0400

    adding schools section

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,12 @@ import Image from "next/image";
 import styles from "../styles/page.module.css";
 import Footer from "@/components/Footer";
 import CommunitySection from "@/components/CommunitySection";
+import UniversitiesSection from "@/components/UniversitiesSection";
 
 export default function Home() {
   return (
     <>
+      <UniversitiesSection />
       <CommunitySection />
       <Footer />
     </>
