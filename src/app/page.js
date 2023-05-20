84a8c37c05783cdc309b7c6837e335a808f4a2ac
SEBAS <scastiblanco2003@gmail.com>
2023-05-20 01:08:02 -0500

    creado scrool para las carreras

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,15 @@ import { Header } from "@/components/header/Header";
 import styles from "../styles/page.module.css";
 import Footer from "@/components/Footer/Footer";
 import CommunitySection from "@/components/CommunitySection/CommunitySection";
+import Carrers from "../components/careers/careers.jsx"
+
+
 export default function Home() {
   return (
     <>
      <Nav />
       <Header />
+      <Carrers />
       <CommunitySection />
       <Footer />
     </>
