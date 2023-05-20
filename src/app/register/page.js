8a7cd95f874c0cfedc944ff94b3ac6e8275b2064
eufanzky <eufanzky@gmail.com>
2023-05-20 13:40:52 -0400

    adding functionality to links of login and register

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,12 +1,16 @@
+"use client"
+
 import React from "react";
 import styles from "../../styles/register.module.css";
 import dynamic from "next/dynamic";
 import RegisterForm from "@/components/RegisterForm/RegisterForm";
+import Link from "next/link";
 
 const RegisterPage = () => {
   return (
     <section className={styles.section}>
       <div className={styles.login_box}>
+        <Link href="/" className={styles.return_button}>{"<-"}</Link>
         <h2 className={styles.title}>Registro</h2>
         <RegisterForm />
       </div>
