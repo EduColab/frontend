c6b1fb7a00036b5832a835f6c2f357ec79fd9127
Uzy <uzielmedina@hotmail.om>
2023-05-21 16:51:17 -0600

    Se guardo la información en localStorage y se pone tu username en el header

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -3,18 +3,47 @@ import React from "react";
 import Link from "next/link";
 import styles from "./Nav.module.css";
 const Nav = () => {
+  const username = localStorage.getItem("user");
+  
+  
   return (
     <header className={styles.header}>
-      <p className={styles.logo}><Link href="/" className={styles.link}>EduColab</Link></p>
+      <p className={styles.logo}>
+        <Link href="/" className={styles.link}>
+          EduColab
+        </Link>
+      </p>
       <nav className={styles.nav}>
-        <Link href="/about" className={styles.link}>Nosotros</Link>
-        <Link href="/community" className={styles.link}>Comunidad</Link>
-        <Link href="/" className={styles.link}>Escuelas</Link>
+        <Link href="/about" className={styles.link}>
+          Nosotros
+        </Link>
+        <Link href="/community" className={styles.link}>
+          Comunidad
+        </Link>
+        <Link href="/" className={styles.link}>
+          Escuelas
+        </Link>
       </nav>
       <div className={styles.logInfo}>
-        <Link href="/login" className={styles.link}>Iniciar sesión</Link>
+        {username ? (
+          <p className={styles.link}>{username}</p>
+        ) : (
+          <Link href="/login" className={styles.link}>
+            Iniciar sesión
+          </Link>
+        )}
+
         <span>|</span>
-        <Link href="/register" className={styles.link}>Registrate</Link>
+        {username ? (
+          <p className={styles.link} onClick={()=>{
+            localStorage.removeItem('user')
+            window.location.href = "/";
+          }}>cerrar sesión</p>
+        ) : (
+          <Link href="/register" className={styles.link}>
+            Registrate
+          </Link>
+        )}
       </div>
     </header>
   );
