a253371c82f08ab47c18ec39f187c17cc204c59f
eufanzky <eufanzky@gmail.com>
2023-05-22 10:51:06 -0400

    Addinf functionality to Form for uploading courses

--- src/services/postCourseService.js
+++ src/services/postCourseService.js
@@ -2,10 +2,9 @@ import axios from "axios";
 export const postCourseService = async (name, description) => {
   const userTemp = localStorage.getItem("userTemp");
   const token = localStorage.getItem("token");
-  const url = `${process.env.NEXT_PUBLIC_API_URL}/courses/`;
+  const url = `${process.env.NEXT_PUBLIC_API_URL}/courses/?type=community`;
   console.log(url);
-  const data = `name=${name}&description=${description}&type=${"community"}&
-  owner=${"admin@solucionesdeingenio.com"}`;
+  const data = `name=${name}&description=${description}&type=${"community"}&owner=${"admin@solucionesdeingenio.com"}`;
 
   const options = {
     method: "POST",
@@ -19,8 +18,6 @@ export const postCourseService = async (name, description) => {
   console.log(options);
   const result = await axios(options);
   if (result.status === 200) {
-    localStorage.setItem("token", result.data.token);
-    localStorage.setItem("user", userTemp);
     window.location.href = "/community"; // or whatever route you want to redirect to
   }
 
